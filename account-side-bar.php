a67a566ccbdf46efde83d51e8bf36c21d2c7cdea
AmmarAlam <ammaralam777@gmail.com>
2016-07-15 22:44:22 +0500

    files updated

---
+++ account-side-bar.php
@@ -0,0 +1,16 @@
+	
+	<div class="side-bar">
+		<div class="profile-img">
+			<img src="images/profile-img.png" style="margin-top:30px;">
+		</div>
+		<h1 class="profile-img-name">Ammar Alam</h1>
+
+		<button class="side-bar-btn btn-img-user">Profile</button>
+		
+		<button class="side-bar-btn btn-img-trip" style="border-bottom:1px solid #ccc; border-top:none;">My Trip</button>
+		
+		<button class="side-bar-btn btn-img-payment" style="border-bottom:1px solid #ccc; border-top:none;">Book Now</button>
+		
+		<button class="side-bar-btn btn-img-signout" style="border-bottom:1px solid #ccc; border-top:none;">Sign Out</button>
+
+	</div>
