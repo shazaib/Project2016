9d7f07c8103d5b1d5fbf1643997baab87301a1a3
shazaib <shazaib.shez11@gmail.com>
2016-07-18 00:50:17 -0700

    Update account-side-bar.php

--- account-side-bar.php
+++ account-side-bar.php
@@ -9,12 +9,11 @@
 		
 		<button class="side-bar-btn btn-img-trip" style="border-bottom:1px solid #ccc; border-top:none;" onclick="location.href='mytrip'">My Trip</button>
 		
-<<<<<<< HEAD
+
 		<button class="side-bar-btn btn-img-payment" style="border-bottom:1px solid #ccc; border-top:none;" onclick="location.href='book-now'">Book Now</button>
-=======
+
 		<button class="side-bar-btn btn-img-booking" style="border-bottom:1px solid #ccc; border-top:none;" onclick="location.href='book-now.php'">Book Now</button>
->>>>>>> 514a1a68ae3419f3603d56a745f5aa8210391546
-		
+
 		<button class="side-bar-btn btn-img-signout" style="border-bottom:1px solid #ccc; border-top:none;">Sign Out</button>
 
 	</div>
